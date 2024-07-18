12e2927eb356e912e36a2357fe88e78ed0e9f4f4
slejennings <sjenni02@calpoly.edu>
2024-07-18 12:44:09 -0700

    cleaned up and checked joins between MUTI and UAI. Start working on UN joins

--- Scripts/Step2_Join_MUTI_UAI_UN.R
+++ Scripts/Step2_Join_MUTI_UAI_UN.R
@@ -13,6 +13,7 @@ library(ggplot2)
 library(ggeffects)
 library(sjPlot)
 library(dplyr)
+library(here)
 
 if(!require(here)){
   install.packages("here")
@@ -63,6 +64,7 @@ colnames(MUTI.f)
 
 
 JetzMUTI_UAI <- inner_join(UAI, MUTI.f, by="Species_Jetz") 
+head(JetzMUTI_UAI)
 nrow(JetzMUTI_UAI)
 #330.... 
 
@@ -82,7 +84,7 @@ nrow(BirdLifeMUTI_UAI)
 #404.... 
 
 nrow(MUTI.f)- nrow(BirdLifeMUTI_UAI) 
-#28 species off! prob not Jetz
+#28 species off! This is a possibility, but eBird may do better
 
 
 
@@ -106,78 +108,88 @@ nrow(MUTI.f)- nrow(eBirdMUTI_UAI)
 #let's left_join UAI and MUTI.f! this will add MUTI scores to all 421 species with UAI scientific name equivalent 
 colnames(UAI)
 colnames(MUTI.f)
-UAI_and_MUTI <- left_join(UAI, MUTI.f, by="Species_eBird") 
-nrow(UAI_and_MUTI)
+UAI_and_MUTI <- MUTI.f %>% select(-Species_Jetz, -Species_BirdLife) %>% # drop these columns to avoid duplication when joining
+  rename(CommonName_MUTI = common_name, SciName_MUTI = scientific_name) %>% # make columns labels more clearly attributable to either MUTI or UAI
+  right_join(., UAI, by="Species_eBird") %>% # use right join to keep all rows in UAI and only those in MUTI.f that match UAI
+  rename(CommonName_UAI = CommonName) # also rename column of common names from UAI, so it is clear where this column came from
+  
+nrow(UAI_and_MUTI) # 4347
 View(UAI_and_MUTI)
 colnames(UAI_and_MUTI)
 
 
-#let's find out what 11 species are missing  and then manually join them! 
+# let's find out what 11 species are missing  and then manually join them! 
 
 nomatch_eBird_MUTI <- anti_join (MUTI.f, eBirdMUTI_UAI, by = "Species_eBird")
 nrow(nomatch_eBird_MUTI)
 View(nomatch_eBird_MUTI)
-nomatch_eBird_MUTI$Species_Jetz <- nomatch_eBird_MUTI$scientific_name
-colnames(nomatch_eBird_MUTI)
-nomatch_eBird_MUTI$Species_Jetz.x <- nomatch_eBird_MUTI$Species_Jetz
 
 #manual inspection 
 #8 of the 11 not in UAI 
 #3 of the 11 in UAI but not under eBird names, under Jetz names 
-######what to do about these two species? They are Brandt's Cormorant, Double-crested cormorant, and Pelagic Cormorant, so they ARE Going to be coastal. It would be nice to keep them. 
+###### what to do about these two species? They are Brandt's Cormorant, Double-crested cormorant, and Pelagic Cormorant, so they ARE Going to be coastal. It would be nice to keep them. 
 
-#since they are Jetz Taxonomy names... what if we do a join between nomatch_eBird_MUTI and UAI using Species_Jetz? 
+# since they are Jetz Taxonomy names... we need to do a join between nomatch_eBird_MUTI and UAI using Species_Jetz 
 colnames(UAI_and_MUTI)
 colnames(nomatch_eBird_MUTI)
 nomatch_eBird_MUTI.r <- nomatch_eBird_MUTI %>% 
-  select (MUTIscore, Species_Jetz) 
-nomatch_eBird_MUTI.r$Species_Jetz.x <- nomatch_eBird_MUTI.r$Species_Jetz
+  rename(SciName_MUTI = scientific_name, CommonName_MUTI = common_name) %>%
+  select (MUTIscore, SciName_MUTI, CommonName_MUTI, Species_Jetz) 
 colnames(nomatch_eBird_MUTI.r)
 
 
-#FROM CHAT 
-#this join maneuver works! 
+# Emma - I simplified this section to do the same thing in fewer steps (SLJ)
+# this keeps all the MUTI species, even the ones that do not have UAI scores as we may decide some are coastal 
+# e.g., I think Northwestern crows sometimes forage on fish/intertidal inverts and they are in MUTI but not UAI
 
-UAI_and_MUTI <- UAI_and_MUTI %>%
-  left_join(nomatch_eBird_MUTI %>% select(Species_Jetz.x, MUTIscore), by = "Species_Jetz.x") %>%
-  mutate(MUTIscore = coalesce(MUTIscore.x, MUTIscore.y)) %>%
-  select(-MUTIscore.x, -MUTIscore.y)
+colnames(UAI)
 colnames(UAI_and_MUTI)
+UAI_and_MUTI_finalfew <- UAI %>% rename(CommonName_UAI = CommonName) %>%
+  full_join(., nomatch_eBird_MUTI.r, by="Species_Jetz") %>% # use full join to match cormorants and keep all species from MUTI that are not in UAI
+  filter(!is.na(MUTIscore)) %>% # keep only the rows where species have a MUTI score
+  select(MUTIscore, SciName_MUTI, CommonName_MUTI, Species_eBird, Species_Jetz, Family_Jetz, Order_Jetz,
+         CommonName_UAI, aveUAI, Order_eBird, Species_BirdLife) # reorder columns to match UAI_and_MUTI
+  
+View(UAI_and_MUTI_finalfew)
+# here are the 11 species. The 3 cormorants joined to UAI using Jetz names.
+# the 8 remaining species that are not found in UAI are also included but with no UAI score
+# we need to bind these rows to the UAI_and_MUTI data frame to put everything together
 
-UAI_and_MUTI2 <- left_join(UAI_and_MUTI, nomatch_eBird_MUTI.r, by="Species_Jetz.x") 
-nrow(UAI_and_MUTI2)
-View(UAI_and_MUTI2)
-colnames(UAI_and_MUTI2)
-#MUTIscore.x is the column we want to look at here... we don't care about MUTIscore.y 
+# one last issue to resolve - the 3 cormorants are in both UAI_and_MUTI_finalfew and UAI_and_MUTI
+# we only want to keep the version in UAI_and_MUTI_finalfew as this has the MUTI and UAI score for these species
+# use anti_join to resolve this issue and then bind the two data frames together
 
-#lets select to retain only MUTIscore.x 
+UAI_and_MUTI_all <- anti_join(UAI_and_MUTI, UAI_and_MUTI_finalfew, by = join_by(Species_Jetz,CommonName_UAI, aveUAI)) %>%
+  bind_rows(., UAI_and_MUTI_finalfew)
+  
+nrow(UAI_and_MUTI_all) # 4355
+head(UAI_and_MUTI_all)
 
-UAI_and_MUTI3 <- UAI_and_MUTI2 %>% select (Species_Jetz.x, Family_Jetz, Order_Jetz, CommonName, aveUAI, Species_eBird, Species_eBird, Order_eBird, Species_BirdLife.x, scientific_name, common_name, MUTIscore.x)
-View(UAI_and_MUTI3)
+# perform some checks to make sure everything worked as expected
 
-#manually checked for Pelagic Cormorant, Double-Crested Cormorant, and Brandt's cormorant, and those have been successfully joined!
-#now every MUTI that has a corresponding UAI species is joined! Yay! 
+# how many rows/species have a MUTI score?
+UAI_and_MUTI_all %>% filter(!is.na(MUTIscore)) %>% nrow()
+# 432 which is 421 eBird species, the 3 Jetz cormorant species, and the 8 species from MUTI with no UAI match!
 
+# how many rows/species have a UAI score?
+UAI_and_MUTI_all %>% filter(!is.na(aveUAI)) %>% nrow() 
+# 4347 species
 
-#QUESTION - there are 8 species from MUTI that are not represented by UAI... currently not joined... 
-#should they be? 
-
-MUTI_Check <- UAI_and_MUTI3 %>% filter(!is.na(MUTIscore.x))
-nrow(MUTI_Check)
-#424! which is 421 eBird species and the 3 Jetz cormorant species! 
-
+# how many rows/species have both a UAI and MUTI score?
+UAI_and_MUTI_all %>% filter(!is.na(aveUAI) & !is.na(MUTIscore)) %>% nrow() 
+# 424 species
 
 ##################################################################
 ##################################################################
 ##################################################################
 ##################################################################
-#############now it's time for UN Joining! ######################
+############# now it's time for UN Joining! ######################
 
 UN <- read.csv (here("Data", "HuAndCardosoData.csv"), header = T)
 View(UN)
 UN <- UN %>% select(Species, Urban)
 colnames(UN)
-nrow(UN) #528 rows -- we want to keep as many of these as possible! 
+nrow(UN) # 528 rows -- we want to keep as many of these as possible! 
 
 #create corresponding column names with UAI df 
 UN$Species_eBird <- UN$Species
@@ -185,8 +197,9 @@ UN$Species_BirdLife <- UN$Species
 UN$Species_Jetz <- UN$Species
 colnames(UN)
 
-######Try with eBird##################################
-eBird_UN_MUTI_UAI <- inner_join(UAI, UN, by="Species_eBird") 
+###### Try with eBird ##################################
+eBird_UN_MUTI_UAI <- UN %>% select(-Species_BirdLife, -Species_Jetz) %>%
+  inner_join(UAI_and_MUTI_all, UN, by="Species_eBird") 
 nrow(eBird_UN_MUTI_UAI)
 #337 species --> not the best.... 
 
@@ -194,17 +207,19 @@ nrow(UN)- nrow(eBird_UN_MUTI_UAI)
 #191 species off! not looking like eBird
 
 
-######Try with BirdLife##################################
-BirdLife_UN_MUTI_UAI <- inner_join(UAI, UN, by="Species_BirdLife") 
+###### Try with BirdLife ##################################
+BirdLife_UN_MUTI_UAI <- UN %>% select(-Species_eBird, -Species_Jetz) %>%
+  inner_join(UAI_and_MUTI_all, ., by="Species_BirdLife") 
 nrow(BirdLife_UN_MUTI_UAI)
 #357 species --> not the best.... 
 
 nrow(UN)- nrow(BirdLife_UN_MUTI_UAI) 
 #171 species off! not looking like BirdLife
 
-######Try with Jetz##################################
+###### Try with Jetz ##################################
 
-Jetz_UN_MUTI_UAI <- inner_join(UAI, UN, by="Species_Jetz") 
+Jetz_UN_MUTI_UAI <- UN %>% select(-Species_eBird, -Species_BirdLife) %>%
+  inner_join(UAI_and_MUTI_all, ., by="Species_Jetz") 
 nrow(Jetz_UN_MUTI_UAI)
 View(Jetz_UN_MUTI_UAI)
 #479 species  
@@ -212,18 +227,14 @@ View(Jetz_UN_MUTI_UAI)
 nrow(UN)- nrow(Jetz_UN_MUTI_UAI) 
 #49 species off! it's prob jetz! 
 
-colnames(Jetz_UN_MUTI_UAI)
-Jetz_UN_MUTI_UAI$Species_Jetz.x <- Jetz_UN_MUTI_UAI$Species_Jetz
-colnames(Jetz_UN_MUTI_UAI)
-
 
 ################################################################
 #Since Jetz has the most species match... let's join those 479 species to the df with UAI and MUTI 
 
 #join 
-colnames(UN_UAI_MUTI)
-UN$Species_Jetz.x <- UN$Species_Jetz
-UN_UAI_MUTI <- left_join(UAI_and_MUTI3, UN, by="Species_Jetz.x") 
+
+UN_UAI_MUTI <- UN %>% select(-Species_eBird, -Species_BirdLife) %>%
+  left_join(UAI_and_MUTI_all, .) 
 nrow(UN_UAI_MUTI)
 View(UN_UAI_MUTI)
 
@@ -237,10 +248,11 @@ UN_Check <- UN_UAI_MUTI %>% filter(!is.na(Urban))
 nrow(UN_Check)
 #479 species... thank goodness! this is correct  
 
-#anti-join 
+#anti-join to find 49 species with UN who did not match with UAI using Jetz names
 colnames(UN)
 colnames(Jetz_UN_MUTI_UAI)
-nomatch_Jetz_UN <- anti_join (UN, Jetz_UN_MUTI_UAI, by = "Species_Jetz.x")
+nomatch_Jetz_UN <- Jetz_UN_MUTI_UAI %>% select(Species, Species_Jetz) %>%
+  anti_join (UN, .)
 nrow(nomatch_Jetz_UN)
 View(nomatch_Jetz_UN)
 

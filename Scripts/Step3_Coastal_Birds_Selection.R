f3a5fa3a27362512f10c237421f3f2fa8f270e1c
emmagarrison2 <emgballet@gmail.com>
2024-07-22 12:15:34 -0600

    New Script for Step 3 - coastal birds list

---
+++ Scripts/Step3_Coastal_Birds_Selection.R
@@ -0,0 +1,13 @@
+#######Objective of this script: refine list of all bird species (with UAI, MUTI, and/or UN scores) to COASTAL BIRDS ONLY 
+
+library(ape)
+library(geiger)
+library(nlme)
+library(effects)
+library(ggplot2)
+library(ggeffects)
+library(sjPlot)
+library(dplyr)
+library(here)
+
+#test

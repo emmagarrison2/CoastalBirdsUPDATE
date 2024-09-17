c109ff33dfcec27e25c692c30fed9fe009ff5214
emmagarrison2 <emgballet@gmail.com>
2024-09-16 20:50:39 -0600

    labeled density plot for UN

--- Scripts/Step7_Density_Plots.R
+++ Scripts/Step7_Density_Plots.R
@@ -119,6 +119,7 @@ AllBirds_UAI <- AllIndexes_AllBirds %>%
   filter(!is.na(aveUAI) & is.finite(aveUAI))
 nrow(AllBirds_UAI) #4347
 
+
 AllBirds_UAI_2 <- AllBirds_UAI %>% 
   select(aveUAI, Group)
 colnames(AllBirds_UAI)
@@ -259,14 +260,19 @@ print(MUTI_density)
 saveRDS(MUTI_density, here("Outputs", "MUTI_Density_Comparison.rds"))
 
 
-#######################################################################
-#####density plot for UN 
+
+###################################### ALTERNATIVE OPTION FOR UN ###################################### 
+
+########################## percentages, stacked barplot for UN 
+
 
 #new df with reduced columns of AllIndexes_AllBirds
 
 AllBirds_UN <- AllIndexes_AllBirds %>% 
   filter(!is.na(Urban) & is.finite(Urban))
 nrow(AllBirds_UN) #533
+View(AllBirds_UN)
+colnames(AllBirds_UN)
 
 AllBirds_UN_2 <- AllBirds_UN %>% 
   select(Urban, Group)
@@ -284,6 +290,8 @@ CoastalBirds_UN_2 <- CoastalBirds_UN %>%
 colnames(CoastalBirds_UN_2)
 nrow(CoastalBirds_UN_2) #129, looks good! 
 
+View(CoastalBirds_UN_2)
+
 ###time to bind rows for UAI 
 
 combined_UN_for_density <- bind_rows(AllBirds_UN_2, CoastalBirds_UN_2)
@@ -293,61 +301,17 @@ nrow(combined_UN_for_density) #662
 
 
 
-#histogram to display density of UN values (Urban or Non-Urban)  
-#comparing all_birds distribution and coastal_birds distribution 
+combined_UN_for_density_summary <- combined_UN_for_density %>%
+  group_by(Group, Urban) %>%
+  summarise(count = n()) %>%
+  ungroup() %>%
+  group_by(Group) %>%
+  mutate(percentage = count / sum(count) * 100) %>%
+  mutate(Group_Urban = paste(Group, Urban, sep = " & "))  # Combine Group and Urban for legend
 
-UN_density <- ggplot() + 
-  # Density plots according to groups 
-  geom_histogram(
-    data = combined_UN_for_density, 
-    aes(x = Urban, fill = Group), 
-    alpha = 0.7, 
-    binwidth = 0.3
-  ) +  
-  scale_fill_manual(
-    values = c("All" = "cadetblue1", "Coastal" = "cadetblue")
-  )  + 
-  scale_x_continuous(
-    breaks = c(0, 1),         # Specify the breaks (original values on the x-axis)
-    labels = c("Non-Urban", "Urban")  # Labels to display for each break
-  ) +
-  labs (
-    #title = "Coastal Score Distribution", 
-    x = "UN", 
-    y = ""
-  ) + 
-  theme_classic() + 
-  theme(
-    axis.title = element_text(size = 12),   # Adjust axis title size
-    axis.title.y = element_text(margin = margin(r = 10)) 
-  )
-
-print(UN_density)
-
-saveRDS(UN_density, here("Outputs", "UN_Density_Comparison.rds"))
-
-
-
-
-###################### ARRANGE ###################### 
-###################### SIDE BY SIDE ###################### 
-
-#arrange plots 
-all_density <- grid.arrange(UAI_density, MUTI_density, UN_density, ncol=3, top= "Coastal Score Distributions", left = "Number of Species")
-
-saveRDS(all_density, here("Outputs", "All_Density_Plots.rds"))
-
-#?ggplot
-
-
-
-###################################### ALTERNATIVE OPTION FOR UN ###################################### 
-
-########################## percentages, stacked barplot for UN 
-
+View(combined_UN_for_density_summary)
 
 library(dplyr)
-
 
 UN_stacked_barplot <- ggplot(combined_UN_for_density_summary, aes(x = Group, y = percentage, fill = Group_Urban)) + 
   geom_bar(stat = "identity", position = "fill", color = "black") + 
@@ -359,37 +323,29 @@ UN_stacked_barplot <- ggplot(combined_UN_for_density_summary, aes(x = Group, y =
       "All & 1" = "darkslategray4",        # Color for "All & Urban"
       "Coastal & 0" = "deepskyblue1",  # Color for "Coastal & Nonurban"
       "Coastal & 1" = "deepskyblue4"        # Color for "Coastal & Urban"
-    ),
-    labels = c(
-      "All & 0" = "Non-urban",    # Custom labels
-      "All & 1" = "Urban",
-      "Coastal & 0" = "Non-urban",
-      "Coastal & 1" = "Urban"
     )
   ) + 
   labs(
-    x = "", 
+    x = "UN", 
     y = "Percentage", 
     fill = ""  # Legend title
   ) + 
-  guides(
-    fill = guide_legend(nrow = 2)  # Split the legend into 2 rows
-  ) + 
   theme_classic() + 
   theme(
-    axis.title = element_text(size = 12),   # Adjust axis title size
-    legend.position = "top",                # Position the legend at the top
-    legend.title = element_text(size = 12), # Adjust legend title size
-    legend.text = element_text(size = 8)   # Adjust legend text size
+   legend.position = "none"
   )
+
 print(UN_stacked_barplot)
 
 
+UN_stacked_barplot_labeled <- UN_stacked_barplot + annotate("text", x = c(1, 1, 2, 2), y = c(0.2, 0.7, 0.17, 0.65), label = c("Urban", "Non-Urban", "Urban", "Non-Urban"))
+
+print(UN_stacked_barplot_labeled)
 
 
 #new arrangement of all the plots (using stacked, percentage barplot)
 
-all_density_2 <- grid.arrange(UAI_density, MUTI_density, UN_stacked_barplot, ncol=3, top= "Coastal Score Distributions", left = "Number of Species")
+all_density_2 <- grid.arrange(UAI_density, MUTI_density, UN_stacked_barplot_labeled, ncol=3, top= "Coastal Score Distributions", left = "Number of Species")
 
 saveRDS(all_density_2, here("Outputs", "All_Density_Plots_option2.rds"))
 
